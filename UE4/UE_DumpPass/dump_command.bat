a27737c7831304ba202fa99a76342bcc1f7850c5
Damian Andrysiak <andrydamian@gmail.com>
2022-11-05 21:51:37 +0100

    Dump mechanism .bat and command part (#2)
    * attributes change
    
    changed attributes for UE4 and VS
    
    gitnignore update
    
    cleanup, remove useless EXR utils folder
    
    * add a dump commands bats

---
+++ UE4/UE_DumpPass/dump_command.bat
@@ -0,0 +1,1 @@
+"D:\Program Files\Epic\UE4_MASTERS\UE_4.26.2Source\Engine\Binaries\Win64\UE4Editor.exe" %1 -nosound -noailogging -noverifygc -novsync -fps=30 -deterministic -RenderOffscreen -ExecCmds="r.SetRes 1920x1080f, r.BufferVisualizationDumpFramesAsHDR 1, r.DumpPass.Allow 1"
