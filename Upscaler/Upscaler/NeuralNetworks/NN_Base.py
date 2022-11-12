3c058094033ccf0904893d69157fb3a87beff33c
Damian Andrysiak <andrydamian@gmail.com>
2022-11-12 12:02:31 +0100

    init of Upscaler PyTorch project

---
+++ Upscaler/Upscaler/NeuralNetworks/NN_Base.py
@@ -0,0 +1,10 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+class NN_Base(nn.Module):
+    """description of class"""
+
+
