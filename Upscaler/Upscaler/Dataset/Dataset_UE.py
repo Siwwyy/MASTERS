897a734efd0b4e09e7f2d805ef8f9ff43a9f20ec
Damian Andrysiak <andrydamian@gmail.com>
2022-11-28 22:22:01 +0100

    add exr file loading: hr and lr in __getitem__ + csv loading

--- Upscaler/Upscaler/Dataset/Dataset_UE.py
+++ Upscaler/Upscaler/Dataset/Dataset_UE.py
@@ -1,10 +1,17 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms
+import numpy as np
+
+# EXR Utils
+import OpenEXR
+import Imath
+
+# ------------- #
 
 from Config.Config import TensorType, PathType
 from Dataset.Dataset_Base import Dataset_Base
-from typing import Optional
+from typing import Optional, Tuple, Union
 from pathlib import Path
 
 
@@ -25,6 +32,12 @@ class Dataset_UE(Dataset_Base):
     ----------
     """
 
+    folder_structure = {"lr": Path("1920x1080-native"), "hr": Path("3840x2160-TAA")}
+
+    subfolders_structure = {"lr": Path("SceneColor"), "hr": Path("TemporalAA")}
+
+    channels = ["R", "G", "B"]  # for now, support only RGB, maybe alpha in future
+
     def __init__(
         self,
         name: str = "Dataset_UE",
@@ -37,13 +50,21 @@ class Dataset_UE(Dataset_Base):
         ), "Unreal Engine based dataset must contain csv file!"
         super().__init__(name, ds_root_path, csv_root_path, transforms)
 
-        self.lr_folder = ds_root_path / Path("1920x1080-native")
-        self.hr_folder = ds_root_path / Path("3840x2160-TAA")
+        self.lr_folder = (
+            ds_root_path
+            / Dataset_UE.folder_structure["lr"]
+            / Dataset_UE.subfolders_structure["lr"]
+        )
+        self.hr_folder = (
+            ds_root_path
+            / Dataset_UE.folder_structure["hr"]
+            / Dataset_UE.subfolders_structure["hr"]
+        )
 
     def __len__(self) -> int:
         return len(self.csv_file)
 
-    def __getitem__(self, idx: int = None) -> TensorType:
+    def __getitem__(self, idx: int = None) -> Tuple[TensorType, TensorType]:
         assert idx is not None, "Index value can't be None! Should be an integer"
         assert idx < self.__len__(), "Index out of bound"
 
@@ -52,17 +73,51 @@ class Dataset_UE(Dataset_Base):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        print(self.csv_file)
-        print(self.csv_file.iloc[0])
+        file_idx, file_name = self.csv_file.iloc[0]
 
-        # for file_id, file_name in enumerate(Path.iterdir(self.root_dir.absolute())):
-        #    if file_id == idx:
-        #        image = torch.from_numpy(imread(file_name)).permute(2,0,1).float()
-        #        sample = {'image': image}
-        #        if self.transform:
-        #            sample = self.transform(sample)
-        #        return sample
-        # return None
+        abosolute_lr_path = self.lr_folder / file_name
+        abosolute_hr_path = self.hr_folder / file_name
+
+        # TODO WHOLE EXR LOADING UTILITY HAS TO BE MOVED TO SEPARATE FUNCTION e.g., load_exr!
+        # lr file
+        lr_exr_file = OpenEXR.InputFile(str(abosolute_lr_path))
+        lr_dw = lr_exr_file.header()["dataWindow"]
+        size = (lr_dw.max.x - lr_dw.min.x + 1, lr_dw.max.y - lr_dw.min.y + 1)
+
+        # Read each channel of exr file
+        CHANNEL_BUFFER = np.zeros(
+            (len(Dataset_UE.channels), size[0] * size[1]), dtype=np.float16
+        )
+        for channel_idx, channel_buf in enumerate(
+            lr_exr_file.channels(Dataset_UE.channels)
+        ):
+            # If read channel is in supported channels, write it to main np buffer
+            CHANNEL_BUFFER[channel_idx] = np.frombuffer(channel_buf, dtype=np.float16)
+
+        lr_tensor = torch.from_numpy(
+            CHANNEL_BUFFER.reshape(len(Dataset_UE.channels), size[0], size[1])
+        )
+
+        # hr file
+        hr_exr_file = OpenEXR.InputFile(str(abosolute_hr_path))
+        hr_dw = hr_exr_file.header()["dataWindow"]
+        size = (hr_dw.max.x - hr_dw.min.x + 1, hr_dw.max.y - hr_dw.min.y + 1)
+
+        # Read each channel of exr file
+        CHANNEL_BUFFER = np.zeros(
+            (len(Dataset_UE.channels), size[0] * size[1]), dtype=np.float16
+        )
+        for channel_idx, channel_buf in enumerate(
+            hr_exr_file.channels(Dataset_UE.channels)
+        ):
+            # If read channel is in supported channels, write it to main np buffer
+            CHANNEL_BUFFER[channel_idx] = np.frombuffer(channel_buf, dtype=np.float16)
+
+        hr_tensor = torch.from_numpy(
+            CHANNEL_BUFFER.reshape(len(Dataset_UE.channels), size[0], size[1])
+        )
+
+        return (lr_tensor, hr_tensor)  # maybe, return dict?
 
 
 def test_ds_ue():
