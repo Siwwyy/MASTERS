afcc4e4c82f85d63089b03632a4e2b8f9b0e04b6
Damian Andrysiak <andrydamian@gmail.com>
2024-07-17 22:51:04 +0200

    add base encoder, decoder and input processing blocks

--- NN/NN/NN/NeuralNetworkArchitecture/ModelKPN.py
+++ NN/NN/NN/NeuralNetworkArchitecture/ModelKPN.py
@@ -4,7 +4,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from abc import ABCMeta, abstractmethod
+
+from torch.nn.modules import activation
 from Config import TensorType, ShapeType, _NNBaseClass
+from ImageProcessing.Colorspace import rgbToGrayScale
 from ImageProcessing.ImageGradient import calculateGradientMagnitude, _AVAILABLE_KERNELS
 from Dataloader import saveEXR
 
@@ -20,27 +23,88 @@ ANGUS G. FORBES, University of California, Santa Cruz
 
 
 class InputProcessing(_NNBaseClass):
+    """
+    InputProcessing
+    """
+
     def __init__(self):
         pass
 
     def forward(
-        self, input: TensorType = None, output: TensorType = None
+        self, input: TensorType = None, warpedOutput: TensorType = None
     ) -> TensorType:
 
+        assert (
+            input.dim() == 4 and warpedOutput.dim() == 4
+        ), f"Forward function params must be in NCHW (dim==4), but got input: {input.dim()} and warpedOutput: {warpedOutput.dim()}"
+        grayScaleInput = rgbToGrayScale(input)
         gradMagnitude = calculateGradientMagnitude(
             input, _AVAILABLE_KERNELS["sobelKernel"]
         )
 
-        print()
+        return torch.cat(
+            [grayScaleInput, warpedOutput], dim=-3
+        )  # assuming NCHW, concatenate through C channel
 
 
-"""
-Dataclass which encapsulates additional input informations to the model
-"""
+class EncoderBlock(_NNBaseClass):
+    """
+    EncoderBlock
+    """
+
+    def __init__(self):
+        layers = nn.ModuleDict(
+            {
+                "conv3x3": nn.Conv2d(
+                    3, 3, (3, 3)
+                ),  # TODO how many channels are in/out, for now keep 3 as we operate on RGB data
+                "batchNorm": nn.BatchNorm2d(3),
+                "elu": nn.ELU(),
+                "conv3x3": nn.Conv2d(
+                    3, 3, (3, 3)
+                ),  # TODO how many channels are in/out, for now keep 3 as we operate on RGB data
+                "batchNorm": nn.BatchNorm2d(3),
+                "elu": nn.ELU(),
+                "maxPool2x2": nn.MaxPool2d((2, 2)),
+            }
+        )
+
+    def forward(self, input: TensorType = None) -> TensorType:
+        ...
+
+
+class DecoderBlock(_NNBaseClass):
+    """
+    DecoderBlock
+    """
+
+    def __init__(self):
+        layers = nn.ModuleDict(
+            {
+                "conv3x3": nn.Conv2d(
+                    3, 3, (3, 3)
+                ),  # TODO how many channels are in/out, for now keep 3 as we operate on RGB data
+                "batchNorm": nn.BatchNorm2d(3),
+                "elu": nn.ELU(),
+                "conv3x3": nn.Conv2d(
+                    3, 3, (3, 3)
+                ),  # TODO how many channels are in/out, for now keep 3 as we operate on RGB data
+                "batchNorm": nn.BatchNorm2d(3),
+                "elu": nn.ELU(),
+                "maxPool2x2": nn.MaxPool2d((2, 2)),
+            }
+        )
+
+    def forward(self, input: TensorType = None) -> TensorType:
+        ...
 
 
 @dataclass
 class ModelKPNInputs:
+    """
+    Dataclass which encapsulates additional input informations to the model
+    """
+
     inputShape: ShapeType = (1, 3, 1920, 1080)  # NCHW
     outputShape: ShapeType = (1, 3, 1920, 1080)  # NCHW
 
@@ -50,7 +114,7 @@ class ModelKPN(_NNBaseClass):
     ModelKPN QWNET
     """
 
-    def __init__(self, name: str = "Model_Base", modelInputs: ModelKPNInputs = None):
+    def __init__(self, name: str = "ModelKPN", modelInputs: ModelKPNInputs = None):
         super().__init__()
         self.name = name
         self.inputShape = modelInputs.inputShape
